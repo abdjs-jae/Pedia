82a4cd7f32c505c45cd0fbdd2914af8c9fbc6f9f
Jason Abadejos <jason_abadejos@dlsu.edu.ph>
2017-02-22 21:39:22 +0800

    Initialized spinner data

--- app/src/main/java/app/rocketship/pedia/utils/SliderActivity.java
+++ app/src/main/java/app/rocketship/pedia/utils/SliderActivity.java
@@ -7,7 +7,11 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import app.rocketship.pedia.MenuActivity;
 import app.rocketship.pedia.R;
@@ -28,11 +32,32 @@ public class SliderActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.sliderdotlayout);
         spinnerMenu = (Spinner) findViewById(R.id.menu_spinner);
 
-
+        initializeSpinner();
 
         pagerAdapter = new SliderFragmentPagerAdapter(getSupportFragmentManager(), MenuActivity.selectedPage);
         sliderViewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(sliderViewPager, true);
+    }
+
+    private void initializeSpinner(){
+
+        // Create arraylist for the spinner
+        List<String> list = new ArrayList<String>();
+        list.add(getString(R.string.brezu));
+        list.add(getString(R.string.brezu_video));
+        list.add(getString(R.string.clarithromycin));
+        list.add(getString(R.string.dibher));
+        list.add(getString(R.string.dibher1mg));
+        list.add(getString(R.string.kidzkit));
+        list.add(getString(R.string.montemax));
+        list.add(getString(R.string.natravox));
+        list.add(getString(R.string.corpvideo));
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMenu.setAdapter(dataAdapter);
+
     }
 
     @Override
